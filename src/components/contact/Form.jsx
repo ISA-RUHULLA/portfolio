c28deacb832ab64aa936ef85f63e11c8741496ea
unknown <isaruhullah035@gmail.com>
2025-12-06 17:52:24 +0600

    Update env variables for Vercel

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -107,10 +107,10 @@ const Form = () => {
 
     emailjs
       .sendForm(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         formRef.current,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
       .then(
         (result) => {
